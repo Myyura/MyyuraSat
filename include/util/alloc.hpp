eefc760a7185f8e7084baf8864300964d69e1937
Myyura <zz940521@gmail.com>
2018-09-15 17:36:08 +0900

    A simple region-based memory allocator, alloc.hpp

--- include/util/alloc.hpp
+++ include/util/alloc.hpp
@@ -7,6 +7,7 @@
 
 #include <stdexcept>
 #include <new>
+#include <limits>
 #include <cstdlib>
 #include <cstdint>
 
@@ -20,17 +21,40 @@ private:
     std::uint32_t _capacity;
     std::uint32_t _wasted;
 
-    void _realloc(uint32_t min_cap);
+    void reserve(uint32_t min_cap) {
+        if (_capacity >= min_cap) { return; }
+
+        uint32_t prev_cap = _capacity;
+        for (; _capacity < min_cap;) {
+            /**
+             * NOTE: Multiply by a factor (13/8) without causing overflow, then 
+             * add 2 and make the result even by clearing the least significant 
+             * bit. The resulting sequence of capacities is carefully chosen to 
+             * hit a maximum capacity that is close to the '2^32-1' limit when
+             * using 'uint32_t' as indices so that as much as possible of this 
+             * space can be used.
+             */
+            uint32_t delta = ((_capacity >> 1) + (_capacity >> 3) + 2) & ~1;
+            _capacity += delta;
+
+            if (_capacity <= prev_cap) {
+                throw std::bad_alloc();
+            }
+        }
+
+        _memory = (T *)std::realloc(_memory, UNIT_SIZE * _capacity);
+        if (_memory == NULL) { throw std::bad_alloc(); }
+    }
 
 public:
     const std::size_t UNIT_SIZE = sizeof(T);
 
     using RARef = std::uint32_t;
-    const RARef RAREF_UNDEF = UINT32_MAX;
+    const RARef RAREF_UNDEF = std::numeric_limits<uint32_t>::max();
 
     explicit RegionAllocator(std::uint32_t start_cap = 1024 * 1024):
         _memory(NULL), _size(0), _capacity(0), _wasted(0) {
-        _realloc(start_cap);
+        reserve(start_cap);
     }
 
     ~RegionAllocator(void) { if (_memory != NULL) { std::free(_memory); } }
@@ -38,7 +62,21 @@ public:
     std::uint32_t size(void) const { return _size; }
     std::uint32_t wasted(void) const { return _wasted; }
 
-    RARef alloc (int size);
+    RARef alloc (int size) {
+        if (size <= 0) {
+            throw std::invalid_argument("RegionAllocator<T>::alloc(int size) : the argument size must be greater than 0");
+        }
+
+        reserve(_size + size);
+
+        uint32_t prev_size = _size;
+        _size += size;
+
+        // Handle overflow
+        if (_size < prev_size) { throw std::bad_alloc(); }
+
+        return prev_size;
+    }
     void free(int size) { _wasted += size; }
 
     // Deref, Load Effective Address (LEA), Inverse of LEA (AEL):
@@ -91,49 +129,6 @@ public:
         _size = _capacity = _wasted = 0;
     }
 };
-
-template<class T>
-void RegionAllocator<T>::_realloc(uint32_t min_cap) {
-    if (_capacity >= min_cap) { return; }
-
-    uint32_t prev_cap = _capacity;
-    for (; _capacity < min_cap;) {
-        /**
-         * NOTE: Multiply by a factor (13/8) without causing overflow, then 
-         * add 2 and make the result even by clearing the least significant 
-         * bit. The resulting sequence of capacities is carefully chosen to 
-         * hit a maximum capacity that is close to the '2^32-1' limit when
-         * using 'uint32_t' as indices so that as much as possible of this 
-         * space can be used.
-         */
-        uint32_t delta = ((_capacity >> 1) + (_capacity >> 3) + 2) & ~1;
-        _capacity += delta;
-
-        if (_capacity <= prev_cap) {
-            throw std::bad_alloc();
-        }
-    }
-
-    _memory = (T *)std::realloc(_memory, UNIT_SIZE * _capacity);
-    if (_memory == NULL) { throw std::bad_alloc(); }
-}
-
-template<class T>
-typename RegionAllocator<T>::RARef RegionAllocator<T>::alloc(int size) {
-    if (size <= 0) {
-        throw std::invalid_argument("RegionAllocator<T>::alloc(int size) : the argument size must be greater than 0");
-    }
-
-    _realloc(_size + size);
-
-    uint32_t prev_size = _size;
-    _size += size;
-
-    // Handle overflow
-    if (_size < prev_size) { throw std::bad_alloc(); }
-
-    return prev_size;
-}
 
 };
 
